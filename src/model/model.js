53bdb1d1321dd7111b524853e798131424bb74ab
akshaypx <akshaysrivastava46@gmail.com>
2024-09-16 23:18:07 +0530

    fix: mongo timestamps added

--- src/model/model.js
+++ src/model/model.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    required: true,
-    type: String,
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+    },
+    content: {
+      required: true,
+      type: String,
+    },
+    category: {
+      required: true,
+      type: String,
+    },
+    tags: {
+      required: true,
+      type: [String],
+    },
   },
-  content: {
-    required: true,
-    type: String,
-  },
-  category: {
-    required: true,
-    type: String,
-  },
-  tags: {
-    required: true,
-    type: [String],
-  },
-});
+  { timestamps: true }
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 export default Blog;
